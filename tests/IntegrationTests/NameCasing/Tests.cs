25f59960ef144c55d0c7fe5ac02d04d7c277adad
blokyk <eliot.courvoisier@gmail.com>
2023-11-12 14:19:35 +0100

    nit: refactor name-casing tests

--- tests/IntegrationTests/NameCasing/Tests.cs
+++ tests/IntegrationTests/NameCasing/Tests.cs
@@ -3,46 +3,41 @@ using StarKid.Generated;
 namespace StarKid.Tests.NameCasing;
 
 public class Tests {
-    [Fact]
-    public void Opts() {
-        // note: we use Last in case the opt name is in the usage
-        string optLine;
-        var helpLines = StarKidProgram.MainHelpText.Split('\n');
+    // todo: rewrite a few tests to use [Theory]/[InlineData] where possible
 
-        var optionsSections
-            = helpLines
+    static readonly string[] _mainHelpLines = StarKidProgram.MainHelpText.Split('\n');
+    static readonly string[] _mainOptionsSection
+        = _mainHelpLines
                 .SkipWhile(s => !s.Contains("Options:"))
                 .Skip(2) // skip "Options:" line and then -h/--help line
                 .TakeWhile(s => !String.IsNullOrWhiteSpace(s))
                 .ToArray();
 
-        Assert.Contains("--opt1 <SOME-VAL>", optionsSections[0]);
-
-        optLine = optionsSections.First(s => s.Contains("--opt2"));
-        Assert.Contains("--opt2 <URL-MAX-LENGTH>", optionsSections[1]);
-
-        optLine = optionsSections.First(s => s.Contains("--opt3"));
-        Assert.Contains("--opt3 <S-SOME-NIGHTMARISH-VAR-NAME>", optionsSections[2]);
+    [Theory]
+    [InlineData(0, "SOME-VAL")]
+    [InlineData(1, "URL-MAX-LENGTH")]
+    [InlineData(2, "S-SOME-NIGHTMARISH-VAR-NAME")]
+    public void Opts(int i, string expected) {
+        Assert.Contains($"--opt{i+1} <{expected}>", _mainOptionsSection[i]);
     }
 
-    [Fact]
-    public void Args() {
-        var helpLines = StarKidProgram.DummyHelpText.Split('\n');
 
-        var usage = helpLines[1];
-        Assert.Contains("<SOME-VAL>", usage);
-        Assert.Contains("<URL-MAX-LENGTH>", usage);
-        Assert.Contains("<S-SOME-NIGHTMARISH-VAR-NAME>", usage);
-
-        var argumentsSection
-            = helpLines
+    public static readonly string[] _dummyHelpLines = StarKidProgram.DummyHelpText.Split('\n');
+    public static readonly string[] _dummyArgsSection
+        = _dummyHelpLines
                 .SkipWhile(s => !s.Contains("Arguments:"))
                 .Skip(1)
                 .TakeWhile(s => !String.IsNullOrWhiteSpace(s))
                 .ToArray();
 
-        Assert.Contains("SOME-VAL", argumentsSection[0]);
-        Assert.Contains("URL-MAX-LENGTH", argumentsSection[1]);
-        Assert.Contains("S-SOME-NIGHTMARISH-VAR-NAME", argumentsSection[2]);
+    [Theory]
+    [InlineData(0, "SOME-VAL")]
+    [InlineData(1, "URL-MAX-LENGTH")]
+    [InlineData(2, "S-SOME-NIGHTMARISH-VAR-NAME")]
+    public void Args(int i, string expected) {
+        var usage = _dummyHelpLines[1];
+        Assert.Contains($"<{expected}>", usage);
+
+        Assert.Contains(expected, _dummyArgsSection[i]);
     }
 }
