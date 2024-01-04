0bf08f18f61079b872a69ea5f0c96c6cf2f1605a
blokyk <eliot.courvoisier@gmail.com>
2024-01-04 21:03:10 +0100

    nit: merge AttrListInfo.cs into builder + mv out of SymbolModel

--- src/AttributeListBuilder.cs
+++ src/AttributeListBuilder.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics;
 
-namespace StarKid.Generator.SymbolModel;
+namespace StarKid.Generator;
 
 internal enum CLIMemberKind {
     None,
@@ -9,6 +9,28 @@ internal enum CLIMemberKind {
     Option,
     Command,
     Invalid
+}
+
+internal readonly record struct AttributeListInfo(
+    CommandGroupAttribute? CommandGroup,
+    CommandAttribute? Command,
+    OptionAttribute? Option,
+    ParseWithAttribute? ParseWith,
+    ImmutableValueArray<ValidateWithAttribute> ValidateWithList,
+    bool IsOnParameter
+)
+{
+    internal bool IsEmpty
+        => CommandGroup is null
+        && Command is null
+        && Option is null
+        && ParseWith is null
+        && ValidateWithList.Length == 0
+        ;
+    internal bool IsUninitialized
+        => ValidateWithList.Array.IsDefault;
+
+    internal CLIMemberKind Kind => AttributeListBuilder.CategorizeAttributeList(this);
 }
 
 internal class AttributeListBuilder
